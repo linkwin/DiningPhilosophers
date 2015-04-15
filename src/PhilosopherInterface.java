e8aa15819790c1586f005138376f8f7f5939ff9a
crgriser <crgriser1@catamount.wcu.edu>
2015-04-15 17:03:37 -0400

    Revert "codebase"
    This reverts commit 440e2a1691c8788809c96e2e241ad52abec2650e.

--- src/PhilosopherInterface.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Alan.
- * @version 4/14/2015.
- */
-public interface PhilosopherInterface {
-    public static final int DINERS = 5;
-    public void takeChopsticks();
-    public void replaceChopsticks();
-}
--- src/State.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Alan.
- * @version 4/14/2015.
- */
-public enum State {
-    THINKING, EATING, HUNGRY
-}
